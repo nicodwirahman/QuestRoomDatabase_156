349733e2f81df13660350ed417c34ef674cf2ca8
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-16 20:47:24 +0700

    memperbaiki agara bisa menjalankan pengelolaHalaman

--- app/src/main/java/com/example/meet9/MainActivity.kt
+++ app/src/main/java/com/example/meet9/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -12,26 +13,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.meet9.ui.navigation.PengelolaHalaman
 import com.example.meet9.ui.theme.Meet9Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // Pastikan metode ini valid dan berfungsi sesuai keinginan Anda
+
         setContent {
             Meet9Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    Scaffold
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                // Menggunakan Scaffold untuk mengatur layout
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PengelolaHalaman(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
