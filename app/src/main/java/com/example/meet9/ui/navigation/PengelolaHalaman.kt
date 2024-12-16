cae0a4b27377d33808c2becf1b1626c64680c6a8
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-17 02:53:10 +0700

    membuat pengelola halaman HomeMhsView

--- app/src/main/java/com/example/meet9/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/meet9/ui/navigation/PengelolaHalaman.kt
@@ -3,28 +3,46 @@ package com.example.meet9.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.meet9.ui.view.mahasiswa.HomeMhsView
 import com.example.roomdatabase.ui.theme.view.mahasiswa.DestinasiInsert
 import com.example.roomdatabase.ui.theme.view.mahasiswa.InsertMhsView
 
+
+
 @Composable
 fun PengelolaHalaman(
-    navController: NavController = rememberNavController(),
+    navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
-){
-    NavHost(
-        navController = NavController,
-        startDestination = DestinasiInsert.route
-    ) {
+) {
+    NavHost(navController = navController,startDestination = DestinasiInsert.route) {
         composable(
             route = DestinasiInsert.route
-        ){
-            InsertMhsView(
-                onBack = {},onNavigate = {})
+        ) {
+            HomeMhsView(
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+
+                    println(
+                        "pengelolaHalaman:nim = @nim"
+                    )
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = modifier
+            )
         }
-
-
-
+            composable(
+                route = DestinasiInsert.route
+            ){
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {  }
+            )
+        }
     }
 }
