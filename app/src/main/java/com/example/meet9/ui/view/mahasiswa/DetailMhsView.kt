20b36a9793521fe7d767a687d95b77b7a3d681d4
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-17 01:48:20 +0700

    memebuat tomobol konfirmasi delete

---
+++ app/src/main/java/com/example/meet9/ui/view/mahasiswa/DetailMhsView.kt
@@ -0,0 +1,28 @@
+package com.example.meet9.ui.view.mahasiswa
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = { /* Do nothing */},
+        title = {Text("Delete DAta")},
+        text = { Text("Apakah anda yakin ingin menghapus data?")},
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+
+}
