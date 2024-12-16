d6b09c1a216713fd3b6448a86ae63c307e67ee19
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-17 01:57:41 +0700

    membuat detailmhs

--- app/src/main/java/com/example/meet9/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/meet9/ui/view/mahasiswa/DetailMhsView.kt
@@ -5,15 +5,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -24,10 +30,58 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.meet9.data.entity.Mahasiswa
+import com.example.meet9.ui.costumwidget.CostumTopAppBar
+import com.example.meet9.ui.viewmodel.DetailMhsViewModel
 import com.example.meet9.ui.viewmodel.DetailUiState
+import com.example.meet9.ui.viewmodel.PenyediaViewModel
 import com.example.meet9.ui.viewmodel.toMahasiswaEntity
 
+@Composable
+fun DetailMhsView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onDeleteClick: () -> Unit = { },
+    onEditClick: (String) -> Unit = {}
+){
+    Scaffold (
+        topBar = {
+            CostumTopAppBar(
+                judul = "Detail Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUIState.value.detailUiEvent.nim)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUIState.collectAsState()
+        BodyDetailMhs(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deletMhs()
+                onDeleteClick()
+            }
+        )
+
+    }
+}
 @Composable
 fun BodyDetailMhs(
     modifier: Modifier = Modifier,
