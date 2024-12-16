a7293743099c5cfa81933c49c5edda450c44b559
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-16 21:53:31 +0700

    untuk menghbungkan MahasiswaDao dalam RepositoryMhs

--- app/src/main/java/com/example/meet9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/meet9/repository/LocalRepositoryMhs.kt
@@ -2,13 +2,31 @@ package com.example.meet9.repository
 
 import com.example.meet9.data.dao.MahasiswaDao
 import com.example.meet9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
-class LocalRepositoryMhs (
-    private val mahasiswaDao: MahasiswaDao
-): RepositoryMhs{
-    override suspend fun insertMHS(mahasiswa: Mahasiswa) {
-       mahasiswaDao.insertMahasiswa(mahasiswa)
+abstract class LocalRepositoryMhs
+    (private val mahasiswaDao: MahasiswaDao)  : RepositoryMhs
+{
+    override suspend fun InsertMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+
 }
 
 
