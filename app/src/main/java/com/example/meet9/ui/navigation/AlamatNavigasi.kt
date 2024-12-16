41df1d43cd5ac718e51854a689716425885aa26e
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-16 22:06:05 +0700

    Hierarki navigasi dengan rute dan argumen.

--- app/src/main/java/com/example/meet9/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/meet9/ui/navigation/AlamatNavigasi.kt
@@ -1,5 +1,21 @@
 package com.example.meet9.ui.navigation
 
 interface AlamatNavigasi {
-    val route: String
+    val route: String             //untuk menyimpan routing halaman
+
+}
+object DestinasiHome : AlamatNavigasi{
+    override val route = "home"
+
+}
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+
+    object DestinasiUpdate : AlamatNavigasi {
+        override val route = "detail"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM}"
+    }
 }
