3185cb06291af3bc2d8aba61b414753463d17fd6
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-16 21:48:59 +0700

    menambah fungsi delet update dan insert

--- app/src/main/java/com/example/meet9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/meet9/repository/RepositoryMhs.kt
@@ -1,8 +1,18 @@
 package com.example.meet9.repository
 
 import com.example.meet9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
 
+
     suspend fun insertMHS(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun  deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
