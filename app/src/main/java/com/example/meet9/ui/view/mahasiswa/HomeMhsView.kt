15fe24e155b73d3d636e9946a48e56d6320702ba
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-17 01:03:32 +0700

    fun ListMahasiswa

--- app/src/main/java/com/example/meet9/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/meet9/ui/view/mahasiswa/HomeMhsView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
@@ -23,6 +25,30 @@ import com.example.meet9.data.entity.Mahasiswa
 import org.jetbrains.annotations.ApiStatus.Experimental
 
 
+
+
+@Composable
+fun  LisMahasiswa(
+    listMhs: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit ={ }
+){
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = listMhs,
+            itemContent = { mhs ->
+                CardMhs(
+                    mhs = mhs,
+                    onClick = { onClick(mhs.nim) }
+                )
+            }
+
+        )
+
+    }
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardMhs(
