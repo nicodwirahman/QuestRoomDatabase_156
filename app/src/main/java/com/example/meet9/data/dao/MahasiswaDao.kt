f6e0a7cc701601d2572186192325a43552e9284a
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-16 21:46:27 +0700

    menambah fungsi delet update dan insert

--- app/src/main/java/com/example/meet9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/meet9/data/dao/MahasiswaDao.kt
@@ -1,11 +1,29 @@
 package com.example.meet9.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.meet9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa where nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
+
 
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
